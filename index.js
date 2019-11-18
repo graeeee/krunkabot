6e6f4a9fafcaa4557eae3ee3feeb3542e9705a00
graeeee <46288884+graeeee@users.noreply.github.com>
2019-11-18 17:11:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -52,4 +52,28 @@ client.on("message", async message =>
         }
       }
 });
+client.on("message", async message => {
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let information = (`${prefix}adfasfasdfasdfasdieno`)
+             
+              if(cmd === information){
+                if(message.member.roles.find(role => role.name === "Founders"))
+    let botpfp = client.user.displayAvatarURL;
+    let demoembed = new Discord.RichEmbed()
+    .setThumbnail(botpfp)
+    .setTitle("**Discord Information**")
+    .setColor("#FEE267")
+    .addField("<:raidboss:392574240643219456> **Channel Information**", "\n<#644966571235868685> - Just what it suggests, a general chat. Please no images or videos here.\n\n<#645077178953891850> - List of frequently asked questions, if a question is not answered that you would like to ask create a ticket or DM a mod/founder.\n\n<645098695926218762> - Have a question? Use this channel and react to the message to create a support ticket.\n\n<#645407049462710282> - Any important updates to the discord or special messages we need to get out to the discord server.")
+    .addBlankField()
+    .addField("<:raidboss:392574240643219456> **Roles Information**", "\n<@&645071126539534336> - The creators/founders of the discord, otherwise known as owners.\n\n<@&645073100874186764> - The moderators of the discord, keeps the discord nice and tidy.\n\n<@&645364639798132846> - Discord server booster, given special role for their generosity.\n\n<@&645072655967453188> - Image verified users, can post pictures in the Verified Images category.")
+    .addBlankField()
+    .addField("<:raidboss:392574240643219456> **Basic Rules (Not all included)**", "\n**1 -** No racism\n\n**2 -**No doxing\n\n**3 -** No spam\n\n**4 -** No NSFW images/chats (porn, gore etc.)\n\n**5 -** No harassment")
+    .setTimestamp()
+    .setFooter('Krunker Krosshair', botpfp);
+    message.channel.send(demoembed);
+              }
+});
 client.login(process.env.bot_token);
