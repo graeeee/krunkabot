11c4d89ef441f172487145c875a9e6c9b38e180f
graeeee <46288884+graeeee@users.noreply.github.com>
2020-01-11 15:27:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -153,4 +153,21 @@ client.on('voiceStateUpdate', (oldMember, newMember) =>
       });
     }
 });
+//onduty2
+client.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+ 
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    if(cmd === `${prefix}onduty1`){
+       
+        let botembed = new Discord.RichEmbed()
+        .setColor("#D80808")
+        .addField("On Duty", "*If you would like to go OnDuty, use the command **!onduty** to get notifications on reports.*")
+        .addField("Off Duty", "*If you would like to go OffDuty, use the command **!offduty** to not get notifications on reports.*");
+        message.channel.send(botembed);
+    }
+});
 client.login(process.env.bot_token);
