4d6e46f4d2d377624c3d81c9c901d6dac3aa8430
graeeee <46288884+graeeee@users.noreply.github.com>
2020-01-11 15:11:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const botconfig =require("./botconfig.json");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const ms = require("ms");
-
-client.on("ready", async () => {
-  console.log(`${client.user.username} is online!`);
-
-  client.user.setActivity("the Official Krunker Krosshair Discord!", {type: "WATCHING"});
-
-});
+//10man ban command
+//
 client.on("message", async message =>
 {
     if(message.author.bot)
@@ -21,35 +16,120 @@ client.on("message", async message =>
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
        
-    if(cmd === `${prefix}mute`)
+    if(cmd === `${prefix}10manban`)
     {
        
         let tempUser = message.mentions.members.first();
         let tempTime = args[1];
-        let tempRole = message.guild.roles.find(role => role.name === "Muted");
+        let tempRole = message.guild.roles.find(role => role.name === "no10mans");
+        let tempReason = args[2];
        
-        if(message.member.roles.find(role => role.name === "Founders") ||
-            (message.member.roles.find(role => role.name === "Moderators") ))
+        if(message.member.roles.find(role => role.name === "10mancounsel") ||
+            message.member.roles.find(role => role.name === "Hand of the King") ||
+            message.member.roles.find(role => role.name === "The Iron Throne") ||
+            message.member.roles.find(role => role.name === "The Small Counsel"))
         {
             if(!tempUser)
-                return message.channel.send('Invalid Usage, **!mute <@User#1234> <time>**');
+                return message.channel.send('Invalid Usage, `!10manban <@User#1234> <time> <reason>`');
            
             if(!tempTime)
-                return message.channel.send('Invalid Usage, **!mute <@User#1234> <time>**');
+                return message.channel.send('Invalid Usage, `!10manban <@User#1234> <time> <reason>`');
+
+            if(!tempReason)
+            {
+                tempReason = "N/A";
+            }
            
             await(tempUser.addRole(tempRole.id).catch(console.error));
            
-            message.channel.send(`<@${tempUser.id}> has been muted for ${tempTime}.`)
+            const embed = new Discord.RichEmbed()
+                .setTitle(`User has been banned from 10mans`)
+                .setColor("#008000")
+                .setThumbnail(tempUser.user.avatarURL)
+                .addField(`User:`, `<@${tempUser.id}>`, true)
+                .addField(`Admin:`, `${message.author}`, true)
+                .addField(`Length:`, `${tempTime}`, true)
+                .addField(`Reason:`, `${tempReason}`, true)
+            message.channel.send({embed})
+            const DMembed = new Discord.RichEmbed()
+                .setColor("#008000")
+                .setDescription(`**You have been banned from 10mans, if you would like to appeal or have any questions about the ban, please contact 10mancounsel in the <#421469485610958848> channel.**\n\n**Length:** ${tempTime}\n\n**Admin:** ${message.author}\n\n**Reason:** ${tempReason}`);
+            tempUser.sendMessage(DMembed);
            
             setTimeout(function()
             {
                 tempUser.removeRole(tempRole.id);
+                message.channel.send(`<@${tempUser.id}>'s ban has now expired.`);
             }, ms(tempTime));
         }
         else
         {
-            return;
+            message.channel.send("No permission.");
         }
-      }
+    }
 });
+
+
+
+
+
+//information channel
+//
+client.on("message", async message => {
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let information = (`${prefix}adfasfasdfasdfasdieno`)
+             
+              if(cmd === information){
+    let botpfp = client.user.displayAvatarURL;
+    let demoembed = new Discord.RichEmbed()
+    .setThumbnail(botpfp)
+    .setTitle("**Raidboss Information**")
+    .setColor("#808080")
+    .addField("<:raidboss:392574240643219456> **Channel Information**", "\n<#187595606573842432> - Just what it suggests, a general chat. Please no images or videos here.\n\n<#421469485610958848> - Use the support Channel for any requests or support needs (unbans, reports, 10 man roles, etc)\n\n<#397912711524253696> - Use this channel to find people to play or to get involved in a 10 man\n\n<#372976620257935360> - All memes, images, videos, and other off-topic stuff")
+    .addBlankField()
+    .addField("<:raidboss:392574240643219456> **Roles Information**", "\n<@&190805090464956416> - The Mad King\n\n<@&339524053146664960> - Head Admins\n\n <@&190805575523762176> - Admins (5v5)\n\n<@&445925181966516244> - Admins (surf)\n\n<@&496135647870844928> - 10 man Hosts\n\n<@&482947162947190802> - 10man Regulars/Long Time Players\n\n<@&563707849784426506> - 10man Players\n\nAny other roles, are self assigned, in this channel <#524911345708171285>.")
+    .addBlankField()
+    .addField("<:raidboss:392574240643219456> **Important Links**", "\nCommunity Rules - [__raidboss.org/rules__](http://raidboss.org/rules)\nServer Lineup - [__raidboss.org/servers__](http://raidboss.org/servers)\nServer Demos - [__raidboss.org/demos__](http://raidboss.org/demos)\nSever Bans - [__raidboss.org/bans__](http://raidboss.org/bans)\nAll-time Stats - [__raidboss.org/stats__](http://raidboss.org/stats)\n5v5 Monthly Ladder - [__raidboss.org/ladder__](http://raidboss.org/ladder)\n10 Man Rules - [__raidboss.org/10man__](http://raidboss.org/10man)\nVIP/Donation - [__raidboss.org/vip__](http://raidboss.org/donate)\nApply for Admin - [__raidboss.org/membership__](https://raidboss.org/membership)")
+    .setTimestamp()
+    .setFooter('Raidboss', botpfp);
+    message.channel.send(demoembed);
+              }
+});
+//queue 1
+// Added by SeaC
+
+let getjointime = [0];
+
+function cleanDate(a)
+{
+  var d = new Date(a);
+  var c = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+  return c;
+}
+
+client.on("message", async message =>
+{
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+
+    if(cmd === `${prefix}queue1`)
+    {
+      let membersInChannel = message.guild.members.filter(n => n.voiceChannelID === "665648411822915586");
+      let membersInQueue = membersInChannel.map(n => n.displayName + " (" + cleanDate(getjointime[n]) + ")");
+
+      const embed = new Discord.RichEmbed()
+        .setTitle("Current Queue #1")
+        .setColor("#D80808")
+        .setDescription(membersInQueue.join("\n"))
+        .setTimestamp()
+
+      return message.channel.send({embed});
+    }
 client.login(process.env.bot_token);
