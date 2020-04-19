ff68e2bc877dce06d68e404692dc0ee5926e9573
graeeee <46288884+graeeee@users.noreply.github.com>
2020-04-19 15:35:48 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     
-    let botpfp = bot.user.displayAvatarURL;
+    let botpfp = client.user.displayAvatarURL;
     let demoembed = new Discord.RichEmbed()
     .setThumbnail(botpfp)
     .setTitle("**All Commands**")
@@ -11,6 +11,7 @@ module.exports.run = async (client, message, args) => {
     .addField("!userinfo", "Shows players info.")
     .addField("!serverinfo", "Shows servers info.")
     .addField("!riot (RiotName#ID)", "Link your riot account to our discord.")
+    .addField("!getriot *@user*", "Get other users riot name and ID.")
     .setTimestamp()
     .setFooter('Vice Valorant 10mans/Scrims', botpfp);
     message.channel.send(demoembed);
