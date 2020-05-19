567a2ae8470a3866b293abeded3cc85dd88c2e97
graeeee <46288884+graeeee@users.noreply.github.com>
2020-05-18 21:35:15 -0400

    Update d bump.js

--- commands/d bump.js
+++ commands/d bump.js
@@ -7,28 +7,23 @@ const ms = require("ms");
 
 module.exports.run = async (client, message, args) => {
  
-    let tempTime = ms('1m');
+let tempTime = ms('1m');
 
        
-        const embed = new Discord.RichEmbed()
-            .setColor("#FF0000")
-            .setThumbnail(tempUser.user.avatarURL)
-            .addField(`Server Bumped`, `The server was recently bumped. Timer has been reset`, true)
-            .setTimestamp();
-        message.channel.send({embed})
-        const DMembed = new Discord.RichEmbed()
-       
-        setTimeout(function()
-        {
-            message.channel.send("@ here, do the command **!d bump** as soon as possible. Thanks!");
-        }, ms(tempTime));
-    }
-    else
-    {
-        message.channel.send("Error.");
-    }
-}
+const embed = new Discord.RichEmbed()
+    .setColor("#FF0000")
+    .setThumbnail(tempUser.user.avatarURL)
+    .addField(`Server Bumped`, `The server was recently bumped. Timer has been reset`, true)
+    .setTimestamp();
+message.channel.send({embed})
+const DMembed = new Discord.RichEmbed()
+
+setTimeout(function()
+{
+    message.channel.send("@ here, do the command **!d bump** as soon as possible. Thanks!");
+}, ms(tempTime));
+
 
 module.exports.help = {
-    name: "dbump"
+name: "d bump"
 }
