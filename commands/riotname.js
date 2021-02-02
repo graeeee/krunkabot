dbc229c7e50c2cb3ad0345e581fc69a390749c2d
graeeee <46288884+graeeee@users.noreply.github.com>
2021-02-01 23:16:20 -0500

    Create riotname.js

---
+++ commands/riotname.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    let messageArray = message.content.split(" ");
+    client.on('message', message => {
+        if(message.channel.id === '544706702109442051'){
+        string.substring(string.indexOf("#"));
+        message.client.setNickname(string);
+        }    
+    });
+module.exports.help = {
+        name: "riotname"
+}
+}
