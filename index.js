c7270df7cc7b5ffb6508dde1a58728837bd804b3
graeeee <46288884+graeeee@users.noreply.github.com>
2021-02-02 00:22:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -262,21 +262,20 @@ message.channel.send({ embed });
  client.on("message", async message => {
     
     let prefix = botconfig.prefix;
-    let riot = (`${prefix}riot`)
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     if(message.channel.id === '698328586096214098'){
       if(message.author.bot) return;
-      if (message.content.includes!= (riot)) {
+      if (message.content.includes!= ("!riot")) {
         message.channel.send(`${message.author}, incorrect arguments.`)        
         await message.delete(5000)
       }
       else{
-        if (message.content.includes (riot)) {
-          message.member.setNickname(message.content.replace(riot, ''));     
+        if (message.content.includes ("!riot")) {
+          message.member.setNickname(message.content.replace("!riot", ''));     
           message.channel.send("Nickname set.")
-          await message.delete(5000)
+          await message.delete()
       
         }
       }
