11f81da0c125f7d1a3bfc7acbc0e6c008b6e00c3
graeeee <46288884+graeeee@users.noreply.github.com>
2021-02-10 00:03:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) =>
       if(message.author.bot) return;
       if (message.content.includes ("!riot")) {
           message.member.setNickname(message.content.replace("!riot", ''));     
-         message.channel.type === ("dm") + message.author.sendMessage("Nickname set in Vice Valorant Scrims Discord.");
+         message.channel.type === ("dm") + message.author.sendMessage("Nickname set in Vice Valorant Scrims Discord, If it was set incorrectly, just resend your username and client id again.");
          await message.member.delete(5000)
       }
       else{
