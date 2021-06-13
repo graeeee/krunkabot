f78126d21d2af093afb9371eee721c3c05682722
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-13 00:22:12 -0400

    Update howtoplay.js

--- commands/howtoplay.js
+++ commands/howtoplay.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     let botpfp = client.user.displayAvatarURL;
     let demoembed = new Discord.RichEmbed()
     .setTitle("**How to join 10mans**")
-    .setColor("#808080")
+    .setColor("#fee354")
     .addField("**Step 1**", "Read all the rules on our website (https://vicevalorant.com/10mans)")
     .addField("**Step 2**", "After you are finished read the rules, send a message in the <#822290384964943873> channel saying you have read the rules fully.")
     .addField("**Step 3**", "Post your current riot name and id ***(e.g., username#1234)***, and thats it!")
