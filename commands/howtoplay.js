dbda17b699c385e63522d3f57fcf071ad7f53540
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-13 00:00:43 -0400

    Update howtoplay.js

--- commands/howtoplay.js
+++ commands/howtoplay.js
@@ -8,8 +8,8 @@ module.exports.run = async (client, message, args) => {
     .setTitle("**What you need to do to join our 10mans**")
     .setColor("#808080")
     .setDescription("**(Step 1)** - Read all the rules on our website (https://vicevalorant.com/10mans)\n",
-                    "**(Step 2)** - After you are finished read the rules, send a message in the <#822290384964943873> channel,",
-                    "mentioning  <@826514977099481099> or <@698333343485395004> telling them you have read the rules fully.\n"
+                    "**(Step 2)** - After you are finished read the rules, send a message in the <#822290384964943873> channel, ",
+                    "mentioning  <@826514977099481099> or <@698333343485395004> telling them you have read the rules fully.\n",
                     "**(Step 3)** - Post your current riot name and id ***(e.g., username#1234)***, and thats it!\n\n",
                     "Patiently wait for a host to see your messages, and they will give you access to the lobby voice channels.")
     .setTimestamp()
