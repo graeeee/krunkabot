2842b28f3e52890ca3ec83e6c91e37b4fd88fcd7
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-19 13:54:54 -0400

    Update and rename allgood.js to all good.js

---
+++ commands/all good.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    
+    message.channel.send("All good.");
+}
+    module.exports.help = {
+        name: "all good"
+    }
