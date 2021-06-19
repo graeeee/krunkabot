5368c0ead3d27854b4888ed79dbdbf33e5f22194
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-19 13:57:12 -0400

    Create allgood.js

---
+++ commands/allgood.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+    if(message.content == 'all good') {
+        message.reply('All good.');
+    }
+});
+
