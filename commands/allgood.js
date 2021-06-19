60b364aa70025d1dd8d12316a033a29b85af4e1b
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-19 14:01:34 -0400

    Update allgood.js

--- commands/allgood.js
+++ commands/allgood.js
@@ -1,12 +1,1 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on('message', (message) => {
-    if(message.content == 'all good') {
-        message.reply('All good.');
-    }
-}
-    module.exports.help = {
-        name: "allgood"
-    }
 
