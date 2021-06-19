288a3140fc193b247e2df894af716cf8aa43d27d
graeeee <46288884+graeeee@users.noreply.github.com>
2021-06-19 13:56:12 -0400

    Delete all good.js

--- commands/all good.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-module.exports.run = async (client, message, args) => {
-    
-    message.channel.send("All good.");
-}
-    module.exports.help = {
-        name: "all good"
-    }
