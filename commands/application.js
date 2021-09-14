b2d90401338da8c447f114ba482abdc37ea7a52f
graeeee <46288884+graeeee@users.noreply.github.com>
2021-09-13 23:49:08 -0400

    Update application.js

--- commands/application.js
+++ commands/application.js
@@ -26,7 +26,7 @@ module.exports.run = async (client, message, args) => {
       }
     });
     
-    const appsChannel = client.channels.get.cache((ch) => ch.name === '10-man-apps');
+    const appsChannel = client.channels.cache.get("887160839084335184");
     collector.on("end", (collected, reason) => {
               if (reason === "fulfilled") {
                  let index = 1;
